547ca972b9369d09f5059820e9a73e948906d7f1
revanthappasani <s566914@nwmissouri.edu>
2024-02-13 16:37:06 -0600

    commit

---
+++ S566914WhackMole/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  S566914WhackMole
+//
+//  Created by Appasani Revanth on 2/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
