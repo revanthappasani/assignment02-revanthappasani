d32fb058c1bf2fdb5e8c7e8281e9550031435522
revanthappasani <s566914@nwmissouri.edu>
2024-02-13 18:13:15 -0600

    created cocoa touch class

---
+++ S566914WhackMole/WhackAMoleVC.swift
@@ -0,0 +1,29 @@
+//
+//  WhackAMoleVC.swift
+//  S566914WhackMole
+//
+//  Created by Appasani Revanth on 2/13/24.
+//
+
+import UIKit
+
+class WhackAMoleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
